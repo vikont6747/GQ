31b342b6523dc6f974f06fef5728a3e1e67f7cb8
vikont6747 <barboss8208@gmail.com>
2023-06-24 04:34:45 +0700

    2.1

--- app/src/main/java/com/example/gq/MainActivity.kt
+++ app/src/main/java/com/example/gq/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.gq
 
+import Question
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +29,10 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.true_button)
         falseButton =
             findViewById(R.id.false_button)
+        nextButton =
+            findViewById(R.id.next_button)
+        questionTextView =
+            findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
                 this,
@@ -31,5 +47,16 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT)
                 .show()
         }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) %
+                    questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+    private fun updateQuestion() {
+        val questionTextResId =
+            questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
