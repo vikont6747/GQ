e8bbe675528336dcd080f99e2c4f707ace421420
vikont6747 <barboss8208@gmail.com>
2023-06-24 04:52:13 +0700

    2

--- app/src/main/java/com/example/gq/MainActivity.kt
+++ app/src/main/java/com/example/gq/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var backButton: Button
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -31,24 +32,23 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.false_button)
         nextButton =
             findViewById(R.id.next_button)
+        backButton =
+            findViewById(R.id.back_button)
         questionTextView =
             findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) %
+                    questionBank.size
+            updateQuestion()
+        }
+        backButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) %
                     questionBank.size
             updateQuestion()
         }
@@ -58,5 +58,19 @@ class MainActivity : AppCompatActivity() {
         val questionTextResId =
             questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer:
+                            Boolean) {
+        val correctAnswer =
+            questionBank[currentIndex].answer
+        val messageResId = if (userAnswer ==
+            correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId,
+            Toast.LENGTH_SHORT)
+            .show()
     }
 }
